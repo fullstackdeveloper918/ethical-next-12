f6237555686c67bacfc607d711f8b68c29947f71
shubham <shubham@cybersify.tech>
2024-01-31 10:49:11 +0530

    removed unused/don't need

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,11 @@ import { Provider } from 'react-redux'
 import { builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
 builder.init(builderConfig.apiKey)
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Provider store={store}>
-        <Component {...pageProps} />
+        <Component {...pageProps} suppressHydrationWarning />
       </Provider>
     </div>
   )
