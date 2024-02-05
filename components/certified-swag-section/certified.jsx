31c3430f9206db92904a7db370f1fbe5d47bf422
shubham <shubham@cybersify.tech>
2024-02-05 13:20:45 +0530

    certified swag

--- components/certified-swag-section/certified.jsx
+++ components/certified-swag-section/certified.jsx
@@ -7,15 +7,15 @@ const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 5,
+    items: 6,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 5,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2,
+    items: 3,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -36,9 +36,9 @@ const Certified = () => {
   return (
     <>
       <div className={styles.heading}>
-        Certified <span className={styles.heading_colored}>CORP SWAG</span>
+        Certified <span className={styles.heading_colored}>CORP SWAGa</span>
       </div>
-      <div className="">
+      <div className="" style={{ width: '1800px' }}>
         {data && (
           <div>
             <Carousel
@@ -59,6 +59,7 @@ const Certified = () => {
               // deviceType={this.props.deviceType}
               dotListClass="custom-dot-list-style"
               itemClass="carousel-item-padding-40-px"
+              slidesToSlide={2}
             >
               {data?.length &&
                 data?.map((product) => (
