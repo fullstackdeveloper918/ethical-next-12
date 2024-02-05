09c35a0a34d6243d68c7ff6afc5a7abb7f97dfd3
shubham <shubham@cybersify.tech>
2024-02-05 15:17:29 +0530

    designing dashboard

---
+++ pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LeftSideBar from '../components/admin/left-side-bar/LeftSideBar'
+
+const dashboard = () => {
+  return (
+    <div className="text-green-50">
+      dashboard
+      <LeftSideBar />
+    </div>
+  )
+}
+
+export default dashboard
