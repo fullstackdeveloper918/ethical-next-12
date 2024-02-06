20b171234a804458634a509707f825656da436d1
Abhishek <fullstackdeveloper918@gmail.com>
2024-02-06 11:51:29 +0530

    Dashboard card done

--- components/admin/DashBoard/DashBoard.jsx
+++ components/admin/DashBoard/DashBoard.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Styles from './DashBoard.module.css'
+import { IoSearchOutline } from 'react-icons/io5'
+import { IoIosArrowBack } from 'react-icons/io'
+import { IoIosArrowForward } from 'react-icons/io'
 import Icons from '../Icons/Icons'
 
 const DashBoard = () => {
@@ -15,8 +18,23 @@ const DashBoard = () => {
         </div>
         <div className={Styles.purchase_card}>
           <nav className={Styles.nav_content}>
-            <button>Add Purchase</button>
-            <span>...</span>
+            <div className={Styles.left_section}>
+              <button className={Styles.Add_Purchase_btn}>Add Purchase</button>
+              <span className={Styles.message}>...</span>
+            </div>
+            <div className={Styles.middle_section}>
+              <input type="text" placeholder="Search..." />
+              <button>
+                <span>
+                  <IoSearchOutline />
+                </span>
+              </button>
+            </div>
+            <div className={Styles.right_section}>
+              <p>1-50 of 2,500</p>
+              <IoIosArrowBack />
+              <IoIosArrowForward />
+            </div>
           </nav>
         </div>
       </div>
