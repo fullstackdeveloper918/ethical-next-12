fa6972afb57ec7123011a1c292ea04cccbfeab68
Abhishek <fullstackdeveloper918@gmail.com>
2024-02-19 17:50:44 +0530

    chnages in estimate card

--- components/EstimateCard/EstimateCard.jsx
+++ components/EstimateCard/EstimateCard.jsx
@@ -3,82 +3,79 @@ import Styles from './EstimateCard.module.css'
 import images from '../../constants/images'
 import Image from 'next/image'
 
-const EstimateCard = ({showEstimate}) => {
-  const [quantity, setQuantity] = useState(2)
-  const [buttonIndex, setButtonIndex] = useState(null)
+const EstimateCard = ({ showEstimate }) => {
+  const [quantities, setQuantities] = useState({})
 
   const handleQuantity = (index, type) => {
-    setButtonIndex(index)
-    if (type === 'dec') {
-      console.log('dec clicked')
-      quantity > 1 && setQuantity(quantity - 1)
-      setButtonIndex(index)
-    } else {
-      console.log('inc clicked')
-      setQuantity(quantity + 1)
-      setButtonIndex(index)
-    }
-  }
+    setQuantities((prevQuantities) => {
+      const newQuantities = { ...prevQuantities }
+      console.log('newQuantities', newQuantities)
+      const currentQuantity = newQuantities[index] || 1
 
-  console.log('Estimate Card called')
+      if (type === 'inc') {
+        newQuantities[index] = currentQuantity + 1
+      } else if (type === 'dec') {
+        newQuantities[index] = Math.max(currentQuantity - 1, 1)
+      }
+
+      return newQuantities
+    })
+  }
 
   let arr = ['a', 'b', 'c', 'd', 'e']
   return (
     <>
-   
       <div className={Styles.estimate_wrapper}>
         <h3 className={Styles.estimate_title}>Estimate</h3>
         <div className={Styles.estimate_horizontal_line}></div>
         <div className={Styles.estimate_container}>
           <div className={Styles.estimate_container_top}>
             {arr.map((item, index) => (
-              <>
-                <div className={Styles.estimate_content} key={index}>
-                  <div className={Styles.estimate_content_imgContent}>
-                    <Image
-                      src={images.cart_image}
-                      width={49}
-                      height={66}
-                      alt="product_image"
+              <div className={Styles.estimate_content} key={index}>
+                <div className={Styles.estimate_content_imgContent}>
+                  <Image
+                    src={images.cart_image}
+                    width={49}
+                    height={66}
+                    alt="product_image"
+                  />
+                </div>
+                <div className={Styles.estimate_content_textContent}>
+                  <h4 className={Styles.title}>
+                    Tentree® Men’s Kangaroo Organic Cotton Hoodie
+                  </h4>
+                  <span className={Styles.price}>Price : Starting at $80</span>
+                  <div className={Styles.amountwrapper}>
+                    <div className={Styles.amountContainer}>
+                      <div
+                        className={Styles.decrease_amount}
+                        onClick={() => handleQuantity(index, 'dec')}
+                      >
+                        -
+                      </div>
+
+                      <div className={Styles.amount_number}>
+                        {quantities[index] || 1}
+                      </div>
+
+                      <div
+                        className={Styles.increase_amount}
+                        onClick={() => handleQuantity(index, 'inc')}
+                      >
+                        +
+                      </div>
+                    </div>
+                    <div style={{ cursor: 'pointer' }}>
+                      <Image
+                        src={images.delete_icon}
+                        width={14}
+                        height={16}
+                        alt="delete_icon"
                       />
-                  </div>
-                  <div className={Styles.estimate_content_textContent}>
-                    <h4 className={Styles.title}>
-                      Tentree® Men’s Kangaroo Organic Cotton Hoodie
-                    </h4>
-                    <span className={Styles.price}>
-                      Price : Starting at $80
-                    </span>
-                    <div className={Styles.amountwrapper}>
-                      <div className={Styles.amountContainer}>
-                        <div
-                          className={Styles.decrease_amount}
-                          onClick={() => handleQuantity(index, 'dec')}
-                          >
-                          -
-                        </div>
-
-                        <div className={Styles.amount_number}>{quantity}</div>
-
-                        <div
-                          className={Styles.increase_amount}
-                          onClick={() => handleQuantity(index, 'inc')}
-                          >
-                          +
-                        </div>
-                      </div>
-                      <div style={{ cursor: 'pointer' }}>
-                        <Image
-                          src={images.delete_icon}
-                          width={14}
-                          height={16}
-                          alt="delete_icon"
-                          />
-                      </div>
                     </div>
                   </div>
                 </div>
-              </>
+              </div>
             ))}
           </div>
           <div className={Styles.estimate_container_bottom}>
