6c4304051c3b2ceae76120135aed7945359a15fa
shubham <shubham@cybersify.tech>
2024-02-29 22:06:04 +0530

    changes

--- pages/shipping.jsx
+++ pages/shipping.jsx
@@ -5,17 +5,31 @@ import EstimateCard from '../components/EstimateCard/EstimateCard'
 import Styles from '../components/cart/Cart.module.css'
 import Shipping from '../components/Shipping/Shipping'
 import QuotationSubmissionHeader from '../components/QuotationSubmissionHeader/QuotationSubmissionHeader'
+import { useSelector } from 'react-redux'
+import EmptyContainer from '../components/EmptyContainer/EmptyContainer'
 const shipping = () => {
+  const cartItemsLength = useSelector((state) => state.cart.cartItems.length)
+
   return (
     <>
       <PrimaryHeader />
       <SecondaryHeader />
       <section className={Styles.cart_section}>
-        <div className={Styles.widthFull}>
-          <QuotationSubmissionHeader />
-          <Shipping />
-        </div>
-        {/* <EstimateCard /> */}
+        {/* {cartItemsLength > 0 ? ( */}
+        <>
+          <div className={Styles.widthFull}>
+            <QuotationSubmissionHeader />
+            <Shipping />
+          </div>
+          <EstimateCard />
+        </>
+        {/* ) : (
+          <>
+            <div className={Styles.empty_card_container}>
+              <EmptyContainer />
+            </div>
+          </>
+        )} */}
       </section>
       <Footer />
     </>
