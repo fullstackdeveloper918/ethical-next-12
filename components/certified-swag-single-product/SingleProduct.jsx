6813a0214bfbc413ae766aa50160ceef08972473
Abhishek <fullstackdeveloper918@gmail.com>
2024-03-01 11:47:13 +0530

    changes in single product

--- components/certified-swag-single-product/SingleProduct.jsx
+++ components/certified-swag-single-product/SingleProduct.jsx
@@ -10,44 +10,43 @@ const SingleProduct = ({ product }) => {
 
   const router = useRouter()
   return (
-    <></>
-    // <div
-    //   className={styles.container}
-    //   onClick={() => router.push(`products/${product?.id}`)}
-    // >
-    //   <div className={styles.image_container}>
-    //     {product?.image ? (
-    //       <Image
-    //         src={`/${product?.image}`}
-    //         width={220}
-    //         height={220}
-    //         alt="product_image"
-    //       />
-    //     ) : (
-    //       <>
-    //         <Image
-    //           src={`/${images.No_product}`}
-    //           width={220}
-    //           height={220}
-    //           alt="no product"
-    //         />
-    //       </>
-    //     )}
-    //   </div>
-    //   <div className={styles.product_name}>
-    //     {product?.product_title || 'fghjk'}
-    //   </div>
-    //   <div className={styles.colors_container}>
-    //     {product?.colours &&
-    //       Object.entries(product?.colours).map(([color, imageUrl]) => {
-    //         return (
-    //           <div>
-    //             <Dot color={imageUrl} />
-    //           </div>
-    //         )
-    //       })}
-    //   </div>
-    // </div>
+    <div
+      className={styles.container}
+      onClick={() => router.push(`products/${product?.id}`)}
+    >
+      <div className={styles.image_container}>
+        {product?.image ? (
+          <Image
+            src={`/${product?.image}`}
+            width={220}
+            height={220}
+            alt="product_image"
+          />
+        ) : (
+          <>
+            <Image
+              src={`/${images.No_product}`}
+              width={220}
+              height={220}
+              alt="no product"
+            />
+          </>
+        )}
+      </div>
+      <div className={styles.product_name}>
+        {product?.product_title || 'fghjk'}
+      </div>
+      <div className={styles.colors_container}>
+        {product?.colours &&
+          Object.entries(product?.colours).map(([color, imageUrl]) => {
+            return (
+              <div>
+                <Dot color={imageUrl} />
+              </div>
+            )
+          })}
+      </div>
+    </div>
   )
 }
 
