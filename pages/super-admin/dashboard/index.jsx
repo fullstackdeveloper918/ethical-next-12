fead301b72a83b2dbed53fe4bb89d2f4e46c0e88
shubham <shubham@cybersify.tech>
2024-03-04 17:29:30 +0530

    dashboard 3 api

--- pages/super-admin/dashboard/index.jsx
+++ pages/super-admin/dashboard/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import images from '../../../constants/images'
 import { New_Customers, SuperAdmin_data } from '../../../constants/data'
@@ -8,8 +9,74 @@ import Styles from './dashboard.module.css'
 import { GoDotFill } from 'react-icons/go'
 import { RiDeleteBin6Line } from 'react-icons/ri'
 import { FaRegEdit } from 'react-icons/fa'
+import useFetch from '../../../lib/useFetch'
+import { BsFillBoxFill } from 'react-icons/bs'
+import { IoKeyOutline } from 'react-icons/io5'
+import { CiUser } from 'react-icons/ci'
+import { IoMdCart } from 'react-icons/io'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  getRecentCustomers,
+  getRecentProducts,
+  getTotalCount,
+} from '../../../redux-setup/adminSlice'
+import Loaders from '../../../components/loaders/Loaders'
+import { timeAgo } from '@lib/utils'
 
 const Dashboard = () => {
+  const dispatch = useDispatch()
+  const totalCount = useSelector((state) => state.admin.totalCount)
+  const recentProductsAll = useSelector((state) => state.admin.recentProducts)
+  const recentCustomersAll = useSelector((state) => state.admin.recentCustomers)
+
+  const [
+    count,
+    { response: countResponse, loading: countLoading, error: countError },
+  ] = useFetch(`/sadm/count`, {
+    method: 'get',
+  })
+  const [
+    recentProducts,
+    {
+      response: productsResponse,
+      loading: productsLoading,
+      error: productsError,
+    },
+  ] = useFetch(`/sadm/product/recent`, {
+    method: 'get',
+  })
+  //column_1_retail_price_cad product_title image
+  const [recentCustomers, { response: customerResponse }] = useFetch(
+    `/sadm/customer/recent`,
+    {
+      method: 'get',
+    }
+  )
+
+  useEffect(() => {
+    count()
+    recentProducts()
+    recentCustomers()
+  }, [])
+
+  useEffect(() => {
+    if (countResponse) {
+      dispatch(getTotalCount(countResponse.data))
+    }
+  }, [countResponse])
+
+  useEffect(() => {
+    if (productsResponse) {
+      dispatch(getRecentProducts(productsResponse.data))
+    }
+  }, [productsResponse])
+
+  useEffect(() => {
+    if (customerResponse) {
+      dispatch(getRecentCustomers(customerResponse.data))
+    }
+  }, [customerResponse])
+
   return (
     <>
       <section className={Styles.dashboard_section}>
@@ -24,97 +91,164 @@ const Dashboard = () => {
             <div className={Styles.dashboard_right_content}>
               <Navbar data="Super Admin" thumbnail={images.User_icon} />
               {/* Middle Content */}
-              <div className={Styles.middle_content}>
-                {SuperAdmin_data.map((data) => (
-                  <>
-                    <div className={Styles.container}>
-                      <div className={Styles.content}>
-                        <div>
-                          <span>{data.icon}</span>
-                        </div>
-                        <div>
-                          <p className={Styles.number}>{data.number}</p>
-                          <p className={Styles.text}>{data.text}</p>
-                        </div>
+              {countResponse && Object.keys(totalCount).length > 1 && (
+                <div className={Styles.middle_content}>
+                  <div className={Styles.container}>
+                    <div className={Styles.content}>
+                      <div>
+                        <span>
+                          {' '}
+                          <BsFillBoxFill color="#a2d061" fontSize={30} />
+                        </span>
+                      </div>
+                      <div>
+                        <p className={Styles.number}>
+                          {totalCount.allProductCount}
+                        </p>
+                        <p className={Styles.text}>{'Products'}</p>
                       </div>
                     </div>
-                  </>
-                ))}
-              </div>
+                  </div>
+                  <div className={Styles.container}>
+                    <div className={Styles.content}>
+                      <div>
+                        <span>
+                          {' '}
+                          <IoMdCart color="#a2d061" fontSize={30} />
+                        </span>
+                      </div>
+                      <div>
+                        <p className={Styles.number}>
+                          {' '}
+                          {totalCount.allOrderCount}
+                        </p>
+                        <p className={Styles.text}>{'Orders'}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={Styles.container}>
+                    <div className={Styles.content}>
+                      <div>
+                        <span>
+                          {' '}
+                          <CiUser color="#a2d061" fontSize={30} />
+                        </span>
+                      </div>
+                      <div>
+                        <p className={Styles.number}>
+                          {' '}
+                          {totalCount.allCustomerCount}
+                        </p>
+                        <p className={Styles.text}>{'Customers'}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={Styles.container}>
+                    <div className={Styles.content}>
+                      <div>
+                        <span>
+                          {' '}
+                          <IoKeyOutline color="#a2d061" fontSize={30} />
+                        </span>
+                      </div>
+                      <div>
+                        <p className={Styles.number}>
+                          {' '}
+                          {totalCount.allSaleCount}
+                        </p>
+                        <p className={Styles.text}>{'Sales'}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
 
               {/* Bottom Content */}
               <div className={Styles.bottom_content}>
                 <div className={Styles.bottom_left_content}>
                   <h2>New Product</h2>
-                  <div className={Styles.new_product_table}>
-                    <table>
-                      <tr>
-                        <th>Name</th>
-                        <th>Image</th>
-                        <th>Status</th>
-                        <th>Price</th>
-                        <th>Action</th>
-                      </tr>
-                      {Array.from({ length: 8 }).map((item) => (
-                        <>
+                  {productsLoading ? (
+                    <div className="">
+                      <Loaders />
+                    </div>
+                  ) : (
+                    recentProductsAll && (
+                      <div className={Styles.new_product_table}>
+                        <table>
                           <tr>
-                            <td>Kangaroo Cotton Shirt</td>
-                            <td>
-                              <span>
-                                <Image
-                                  src={images.shirt_small}
-                                  width={25}
-                                  height={25}
-                                  icon="product"
-                                />
-                              </span>
-                            </td>
-                            <td>
-                              <div className={Styles.status}>
-                                <div>
-                                  <span>
-                                    <GoDotFill color="#a2d061" />
-                                  </span>
-                                </div>
-                                <div>
-                                  <span>In Stcok</span>
-                                </div>
-                              </div>
-                            </td>
-                            <td>$80</td>
-                            <td>
-                              <div className={Styles.status}>
-                                <div className={Styles.action}>
-                                  <span>
-                                    <FaRegEdit fontSize={18} />
-                                  </span>
-                                </div>
-                                <div>
-                                  <span>
-                                    <RiDeleteBin6Line fontSize={18} />
-                                  </span>
-                                </div>
-                              </div>
-                            </td>
+                            <th>Name</th>
+                            <th>Image</th>
+                            <th>Status</th>
+                            <th>Price</th>
+                            <th>Action</th>
                           </tr>
+                          {recentProductsAll.map((item) => (
+                            <>
+                              <tr>
+                                <td>{item.product_title}</td>
+                                <td>
+                                  <span>
+                                    <Image
+                                      src={images.shirt_small}
+                                      width={25}
+                                      height={25}
+                                      icon="product"
+                                      // onError={}
+                                    />
+                                  </span>
+                                </td>
+                                <td>
+                                  <div className={Styles.status}>
+                                    <div>
+                                      <span>
+                                        <GoDotFill color="#a2d061" />
+                                      </span>
+                                    </div>
+                                    <div>
+                                      <span>In Stock</span>
+                                    </div>
+                                  </div>
+                                </td>
+                                <td>${item.column_1_retail_price_cad}</td>
+                                <td>
+                                  <div className={Styles.status}>
+                                    <div className={Styles.action}>
+                                      <span>
+                                        <FaRegEdit fontSize={18} />
+                                      </span>
+                                    </div>
+                                    <div>
+                                      <span>
+                                        <RiDeleteBin6Line fontSize={18} />
+                                      </span>
+                                    </div>
+                                  </div>
+                                </td>
+                              </tr>
+                            </>
+                          ))}
+                        </table>
+                      </div>
+                    )
+                  )}
+                </div>
+                {recentCustomersAll && (
+                  <div className={Styles.bottom_right_content}>
+                    <h2>New Customers</h2>
+                    <div className={Styles.newCustomers_data}>
+                      {recentCustomersAll.map((data) => (
+                        <>
+                          <div key={data.id} className={Styles.Customer}>
+                            <p className={Styles.name}>{data.name}</p>
+                            <p className={Styles.time}>
+                              {timeAgo(data.created_at)}
+                            </p>
+                          </div>
                         </>
                       ))}
-                    </table>
+                    </div>
                   </div>
-                </div>
-                <div className={Styles.bottom_right_content}>
-                  <h2>New Customers</h2>
-                  <div className={Styles.newCustomers_data}>
-                    {New_Customers.map((data) => (
-                      <>
-                        <div key={data.id} className={Styles.Customer}>
-                          <p className={Styles.name}>{data.name}</p>
-                          <p className={Styles.time}>{data.time}</p>
-                        </div>
-                      </>
-                    ))}
-                  </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
