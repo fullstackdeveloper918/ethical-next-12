b4779aff121893797ab50450cbdf2d380e83a89c
fullstackdeveloper918 <fullstackdeveloper918@gmail.com>
2024-03-06 16:20:00 +0530

    changes

---
+++ components/Invoice/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Invoice = ({ data }) => {
+  return (
+    <div>
+      <h1>Abhishek</h1>
+      <h1>Abhishek</h1>
+      <h1>Abhishek</h1>
+      <h1>Abhishek</h1>
+      <h1>Abhishek</h1>
+      <h1>final with care</h1>
+      <h1>final with care</h1>
+      <h1>final with care</h1>
+      <h1>final with care</h1>
+      <h1>final with care</h1>
+      <h1>final with care</h1>
+    </div>
+  )
+}
+
+export default Invoice
