05ded637c4d17f9eb4ebe5836662a6bf478d53c3
shubham <shubham@cybersify.tech>
2024-03-07 15:36:45 +0530

    filters change div mani

--- components/certified-swag-single-product/SingleProduct.jsx
+++ components/certified-swag-single-product/SingleProduct.jsx
@@ -1,19 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './singleProduct.module.css'
 import shirtImg from '../../assets/shirt.svg'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Dot from '@components/custom-colored-dot/Dot'
 import images from 'constants/images'
+import { useSelector } from 'react-redux'
 const SingleProduct = ({ product }) => {
   const [starProductsImage, setStarProductImage] = useState(product?.image)
   const router = useRouter()
+  const country = useSelector((state) => state.country.country)
 
-  console.log(product, 'unit price')
+  // console.log(product, 'unit price') //ltm_final column_1_retail_price_usd column_1_retail_price_cad
+  // console.log(product?.column_1_retail_price_usd, 'column_1_retail_price_usd')
+  // console.log(product?.column_1_retail_price_cad, ' column_1_retail_price_cad')
+  // console.log(product?.ltm_final, 'unit ltm_final')
+  //ltm_final column_1_retail_price_usd column_1_retail_price_cad
 
   const handleImageOnError = () => {
     setStarProductImage(images.No_product)
   }
+
+  let isProductIncludesltm_final = product?.ltm_final.includes('Y')
+  let ltmPrice = country === 'usa' ? product?.ltm_usd : product?.ltm_cad
+
+  let col1Price =
+    country === 'usa'
+      ? product?.column_1_retail_price_usd
+      : product?.column_1_retail_price_cad
+
+  // console.log(isProductIncludesltm_final, 'isProductIncludesltm_final')
+
+  const getPrice = () => {
+    let PriceMin
+    if (isProductIncludesltm_final) {
+      let PriceMin = +col1Price + +ltmPrice / 2
+    console.log(PriceMin, 'PriceMin')
+
+    } else if (!isProductIncludesltm_final) {
+      let PriceMin = +col1Price
+    console.log(PriceMin, 'from else')
+
+    }
+  }
+
+  useEffect(() => {
+    getPrice()
+  }, [])
 
   return (
     <div
@@ -30,7 +63,7 @@ const SingleProduct = ({ product }) => {
         />
       </div>
       <div className={styles.product_name}>
-        {product?.product_title || 'fghjk'}
+        {product?.product_title || 'No title received'}
       </div>
       <div className={styles.product_price}>
         <p>{product.unit_price}</p>
