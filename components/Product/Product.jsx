151a09b8c734cf9ff2f6ac27d6d649ce80930b41
shubham <shubham@cybersify.tech>
2024-03-12 19:26:11 +0530

    added not sure about size in single product

--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -27,6 +27,7 @@ const Product = ({ product, loading, error }) => {
     L: 25,
     XL: 25,
   })
+
   const [cartState, setCartState] = useState({
     quantity: 0,
     image: null,
@@ -43,10 +44,9 @@ const Product = ({ product, loading, error }) => {
   const [imagesArray, setImagesArray] = useState([])
   const [singleImage, setSingleImage] = useState(imagesArray[0])
   const [nameOfDecorations, setNameOfDecorations] = useState([])
-
+  const [sizeNotSure, setSizeNotSure] = useState(true)
   const country = useSelector((state) => state.country.country)
   const cartItems = useSelector((state) => state.cart.cartItems)
-
   const decorations = useSelector(
     (state) => state.random.decorationItemObjSingleProductPage
   )
@@ -123,6 +123,18 @@ const Product = ({ product, loading, error }) => {
       setActualMinQty(Math.round(minQtyy))
     }
   }, [product])
+
+  useEffect(() => {
+    if (sizeNotSure) {
+      setSizeQuantity((prev) => ({
+        ...prev,
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0,
+      }))
+    } 
+  }, [sizeNotSure])
 
   useEffect(() => {
     if (product) {
@@ -300,7 +312,6 @@ const Product = ({ product, loading, error }) => {
       checkeeeee()
     }
   }, [product])
-  console.log(product, 'pppprrrooooddduuucccttt') //swift_tag
   return (
     <>
       {loading ? (
@@ -453,44 +464,36 @@ const Product = ({ product, loading, error }) => {
                     </div>
                   </div>
                 )}
-                <div className={Styles.customization_text}>
-                  <div className={Styles.common_header}>
-                    <p>Select Customization</p>
-                    <Image
-                      src={images.Info_Icon}
-                      width={18}
-                      height={18}
-                      alt="info_icon"
-                    />
-                  </div>
+                {Object.keys(finalDecorationKeyVal).length > 0 && (
+                  <div className={Styles.customization_text}>
+                    <div className={Styles.common_header}>
+                      <p>Select Customization</p>
+                      <Image
+                        src={images.Info_Icon}
+                        width={18}
+                        height={18}
+                        alt="info_icon"
+                      />
+                    </div>
 
-                  <div className={Styles.buttons}>
-                    {/* {nameOfDecorations.map((button, index) => (
-                      <button
-                        className={`${Styles.btn} ${
-                          activeBtn === index ? Styles.active : ''
-                        }`}
-                        onClick={() => btnClicked(index, button)}
-                      >
-                        {button}
-                      </button>
-                    ))} */}
-                    {finalDecorationKeyVal &&
-                      Object.keys(finalDecorationKeyVal).length > 0 &&
-                      Object.entries(finalDecorationKeyVal).map(
-                        ([key, val], index) => (
-                          <p
-                            className={`${Styles.btn} ${
-                              activeBtn === index ? Styles.active : ''
-                            }`}
-                            onClick={() => btnClicked(index, key, val)}
-                          >
-                            {val && JSON.parse(val?.decoration_type)}
-                          </p>
-                        )
-                      )}
+                    <div className={Styles.buttons}>
+                      {finalDecorationKeyVal &&
+                        Object.keys(finalDecorationKeyVal).length > 0 &&
+                        Object.entries(finalDecorationKeyVal).map(
+                          ([key, val], index) => (
+                            <p
+                              className={`${Styles.btn} ${
+                                activeBtn === index ? Styles.active : ''
+                              }`}
+                              onClick={() => btnClicked(index, key, val)}
+                            >
+                              {val && JSON.parse(val?.decoration_type)}
+                            </p>
+                          )
+                        )}
+                    </div>
                   </div>
-                </div>
+                )}
                 {/* <div className={Styles.para_text}>
                   <div className={Styles.common_header}>
                     <p>
@@ -641,8 +644,8 @@ const Product = ({ product, loading, error }) => {
                     placeholder={product?.column_1_qty}
                     name="orderQuantity"
                     value={orderQuantity}
-                    // onChange={setQuantity}
-                    disabled
+                    onChange={(e) => setOrderQuantity(e.target.value)}
+                    disabled={!sizeNotSure}
                     min={actualMinQty}
                   />
                   <span>(minimum {+actualMinQty} units required)</span>
@@ -663,34 +666,49 @@ const Product = ({ product, loading, error }) => {
                       placeholder="S"
                       type="number"
                       name="S"
-                      value={sizeQuantity.S}
+                      value={sizeNotSure ? 0 : sizeQuantity.S}
                       onChange={handleQuantitySize}
                       min="0"
+                      disabled={sizeNotSure}
                     />
                     <input
                       placeholder="M"
                       type="number"
                       name="M"
-                      value={sizeQuantity.M}
+                      value={sizeNotSure ? 0 : sizeQuantity.M}
+                      // value={sizeQuantity.M}
                       onChange={handleQuantitySize}
                       min="0"
+                      disabled={sizeNotSure}
                     />
                     <input
                       placeholder="L"
                       type="number"
                       name="L"
-                      value={sizeQuantity.L}
+                      // value={sizeQuantity.L}
+                      value={sizeNotSure ? 0 : sizeQuantity.L}
                       onChange={handleQuantitySize}
                       min="0"
+                      disabled={sizeNotSure}
                     />
                     <input
                       placeholder="XL"
                       type="number"
                       name="XL"
-                      value={sizeQuantity.XL}
+                      // value={sizeQuantity.XL}
+                      value={sizeNotSure ? 0 : sizeQuantity.XL}
                       onChange={handleQuantitySize}
                       min="0"
+                      disabled={sizeNotSure}
                     />
+                  </div>
+                  <div className="">
+                    <input
+                      type="checkbox"
+                      checked={sizeNotSure} //setSizeNotSure
+                      onChange={() => setSizeNotSure(!sizeNotSure)}
+                    />
+                    <label> Not sure about size yet</label>
                   </div>
                 </div>
                 <div className={Styles.standard_business_section}>
