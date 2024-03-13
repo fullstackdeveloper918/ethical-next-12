b58bda45de07f6617e6a4ae6adb49d0d1dfb72ef
Abhishek <fullstackdeveloper918@gmail.com>
2024-03-13 19:57:24 +0530

    changes in orders super admin

--- pages/super-admin/orders/[slug].jsx
+++ pages/super-admin/orders/[slug].jsx
@@ -52,80 +52,316 @@ const slug = () => {
       <div className="container">
         <div className="content">
           <form className={Styles.form}>
+            <h3>Order Details</h3>
             <div className={Styles.input_field}>
-              <label htmlFor="">Subject</label>
+              <label htmlFor="">ORDER ID</label>
               <input
                 type="text"
-                name="Subject"
+                name="ORDER_ID"
                 disabled={IsEditable}
                 placeholder="Subject"
-                value={formData?.Subject}
+                value={formData?.ORDER_ID}
                 onChange={handleChange}
               />
             </div>
             <div className={Styles.input_field}>
-              <label htmlFor="">Owner</label>
+              <label htmlFor="">Client Linked</label>
               <input
                 type="text"
-                name="Owner"
+                name="Client_Linked"
                 disabled={IsEditable}
                 placeholder="Owner"
-                value={formData?.Owner}
+                value={formData?.Client_Linked}
                 onChange={handleChange}
               />
             </div>
             <div className={Styles.input_field}>
-              <label htmlFor="">Activity Type</label>
+              <label htmlFor="">Order Line Items</label>
               <input
                 type="text"
-                name="Activity"
+                name="Order_Line_Items"
                 disabled={IsEditable}
                 placeholder="Activity Type"
-                value={formData?.Activity}
+                value={formData?.Order_Line_Items}
                 onChange={handleChange}
               />
             </div>
             <div className={Styles.input_field}>
-              <label htmlFor="">Status</label>
+              <label htmlFor="">Customer Email</label>
               <input
                 type="text"
-                name="Status"
+                name="Customer_Email"
                 disabled={IsEditable}
                 placeholder="Status"
-                value={formData?.Status}
+                value={formData?.Customer_Email}
                 onChange={handleChange}
               />
             </div>
             <div className={Styles.input_field}>
-              <label htmlFor="">Priority</label>
+              <label htmlFor="">Customer Company</label>
               <input
                 type="text"
-                name="Priority"
+                name="Customer_Company"
                 disabled={IsEditable}
-                placeholder="Priority"
-                value={formData?.Priority}
+                placeholder="Customer Company"
+                value={formData?.Customer_Company}
                 onChange={handleChange}
               />
             </div>
             <div className={Styles.input_field}>
-              <label htmlFor="">Issue Date</label>
+              <label htmlFor="">Customer First Name</label>
               <input
                 type="text"
                 disabled={IsEditable}
-                name="Issue_Date"
-                placeholder="Issue Date"
-                value={formData.Issue_Date}
+                name="Customer_First_Name"
+                placeholder="First Name"
+                value={formData.Customer_First_Name}
                 onChange={handleChange}
               />
             </div>
             <div className={Styles.input_field}>
-              <label htmlFor="">Due Date</label>
+              <label htmlFor="">Customer Last Name</label>
               <input
                 type="text"
-                name="Due_date"
+                name="Customer_Last_Name"
                 disabled={IsEditable}
-                placeholder="Due Date"
-                value={formData.Due_date}
+                placeholder="Last Name"
+                value={formData.Customer_Last_Name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Customer Phone</label>
+              <input
+                type="text"
+                name="Customer_Phone"
+                disabled={IsEditable}
+                placeholder="Phone"
+                value={formData.Customer_Phone}
+                onChange={handleChange}
+              />
+            </div>
+            <h3>Shipping Details</h3>
+
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping First Name</label>
+              <input
+                type="text"
+                name="Shipping_First_Name"
+                disabled={IsEditable}
+                placeholder="Shipping First Name"
+                value={formData.Shipping_First_Name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Last Name</label>
+              <input
+                type="text"
+                name="Shipping_Last_Name"
+                disabled={IsEditable}
+                placeholder="Shipping Last Name"
+                value={formData.Shipping_Last_Name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Company</label>
+              <input
+                type="text"
+                name="Shipping_Company"
+                disabled={IsEditable}
+                placeholder="Shipping Company"
+                value={formData.Shipping_Company}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Address</label>
+              <input
+                type="text"
+                name="Shipping_Address"
+                disabled={IsEditable}
+                placeholder="Shipping Address"
+                value={formData.Shipping_Address}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Address 2</label>
+              <input
+                type="text"
+                name="Shipping_Address_2"
+                disabled={IsEditable}
+                placeholder="Shipping Address 2"
+                value={formData.Shipping_Address_2}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping City</label>
+              <input
+                type="text"
+                name="Shipping_City"
+                disabled={IsEditable}
+                placeholder="Shipping City"
+                value={formData.Shipping_City}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Province</label>
+              <input
+                type="text"
+                name="Shipping_Province"
+                disabled={IsEditable}
+                placeholder="Shipping Province"
+                value={formData.Shipping_Province}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Province Code</label>
+              <input
+                type="text"
+                name="Shipping_Province_Code"
+                disabled={IsEditable}
+                placeholder="Shipping Province Code"
+                value={formData.Shipping_Province_Code}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Country</label>
+              <input
+                type="text"
+                name="Shipping_Country"
+                disabled={IsEditable}
+                placeholder="Shipping Country"
+                value={formData.Shipping_Country}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Country Code</label>
+              <input
+                type="text"
+                name="Shipping_Country_Code"
+                disabled={IsEditable}
+                placeholder="Shipping Country Code"
+                value={formData.Shipping_Country_Code}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Shipping Postal Code</label>
+              <input
+                type="text"
+                name="Shipping_Postal_Code"
+                disabled={IsEditable}
+                placeholder="Shipping Postal Code"
+                value={formData.Shipping_Postal_Code}
+                onChange={handleChange}
+              />
+            </div>
+            <h3>Billing Details</h3>
+
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing First Name</label>
+              <input
+                type="text"
+                name="Billing_First_Name"
+                disabled={IsEditable}
+                placeholder="Billing First Name"
+                value={formData.Billing_First_Name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing Last Name</label>
+              <input
+                type="text"
+                name="Billing_Last_Name"
+                disabled={IsEditable}
+                placeholder="Billing Last Name"
+                value={formData.Billing_Last_Name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing Phone</label>
+              <input
+                type="text"
+                name="Billing_Phone"
+                disabled={IsEditable}
+                placeholder="Billing Phone"
+                value={formData.Billing_Phone}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing Address</label>
+              <input
+                type="text"
+                name="Billing_Address"
+                disabled={IsEditable}
+                placeholder="Billing Address"
+                value={formData.Billing_Address}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing Address 2</label>
+              <input
+                type="text"
+                name="Billing_Address_2"
+                disabled={IsEditable}
+                placeholder="Billing Address 2"
+                value={formData.Billing_Address_2}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing City</label>
+              <input
+                type="text"
+                name="Billing_City"
+                disabled={IsEditable}
+                placeholder="Billing City"
+                value={formData.Billing_City}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing Province</label>
+              <input
+                type="text"
+                name="Billing_Province"
+                disabled={IsEditable}
+                placeholder="Billing Province"
+                value={formData.Billing_Province}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Billing Country Code</label>
+              <input
+                type="text"
+                name="Billing_Country_Code"
+                disabled={IsEditable}
+                placeholder="Billing Country Code"
+                value={formData.Billing_Country_Code}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={Styles.input_field}>
+              <label htmlFor="">Order Number</label>
+              <input
+                type="text"
+                name="Order_Number"
+                disabled={IsEditable}
+                placeholder="Order Number"
+                value={formData.Order_Number}
                 onChange={handleChange}
               />
             </div>
@@ -137,7 +373,15 @@ const slug = () => {
                 </>
               ) : (
                 <>
-                  <button onClick={handleSubmit}>Save Changes</button>
+                  <button type="button" onClick={handleSubmit}>
+                    Save Changes
+                  </button>
+                  <button
+                    onClick={() => router.push('/super-admin/orders')}
+                    type="button"
+                  >
+                    Cancel
+                  </button>
                 </>
               )}
             </div>
