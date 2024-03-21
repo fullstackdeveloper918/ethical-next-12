c41a4ed56a4d57c561c557863b1c6cede3b6c541
shubham <shubham@cybersify.tech>
2024-03-21 13:52:24 +0530

    pagination done

--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -9,17 +9,14 @@ const Pagination = () => {
   const dispatch = useDispatch()
   const [arr, setArr] = useState([])
   const loading = useSelector((state) => state.category.getProductsLoading)
-
   const totalPages = useSelector((state) => state.category.totalPages)
   const page = useSelector((state) => state.category.currentPage)
-  const totalData = useSelector((state) => state.category.totalData)
-  console.log({ totalData, totalPages, page })
 
   const handlePageChange = (val) => {
     if (val === 'prev') {
-      page != 1 && dispatch(setCurrentPage((prev) => prev - 1))
+      page != 1 && dispatch(setCurrentPage(page - 1))
     } else if (val === 'next') {
-      page != totalPages && dispatch(setCurrentPage((prev) => prev + 1))
+      page != totalPages && dispatch(setCurrentPage(page + 1))
     }
   }
   const noOfPages = () => {
@@ -63,7 +60,7 @@ const Pagination = () => {
 
           {arr.map((item, i) => (
             <button
-              onClick={() => setCurrentPage(i + 1)}
+              onClick={() => dispatch(setCurrentPage(i + 1))}
               disabled={loading}
               className={page === i + 1 ? Styles.current_page : ''}
               key={i}
@@ -82,7 +79,8 @@ const Pagination = () => {
             Next
           </button>
           <button
-            className=""
+            onClick={() => handlePageChange('next')}
+            disabled={loading}
             style={{
               opacity: page == totalPages || loading ? '0.7' : '1',
               cursor: 'pointer',
