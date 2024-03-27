803a1ca581c18ba6ce5448e79fec215d10623979
shubham <shubham@cybersify.tech>
2024-03-27 17:36:29 +0530

    socket.io implementedand working

---
+++ pages/check.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SocketComponent from '../components/SocketComponent'
+const check = () => {
+  return (
+    <div>
+      hello bro
+      <div>
+        <h1>Socket.IO Chat App</h1>
+        <SocketComponent />
+      </div>
+    </div>
+  )
+}
+
+export default check
